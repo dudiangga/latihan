db243fa3d166cb11e6213f2b5efb898247ffd1a4
dudiangga <dudiangga12@gmail.com>
2020-01-25 21:39:30 +0700

    no 1

---
+++ 1.js
@@ -0,0 +1,19 @@
+function biodata(name,age)
+{
+    address=console.readline(address);
+    hobies=console.readline(hobies);
+    is_married=console.readkey;
+    list_school=[
+        name_school=console.readline(name_school),
+        year_in=console.readline(year_in),
+        year_out=console.readline(year_out),
+        major=console.readline(major)
+    ];
+    skills=[
+        skill_name=console.readline(skill_name),
+        level=console.readkey(level)
+    ];
+    interes_in_coding
+    return JSON.valueOf(name,age,address,is_married,list_school,skills);
+
+}
